d8638c8e722babe85976b34833716854264e6846
thannedel <57347092+thannedel@users.noreply.github.com>
2021-02-17 14:23:21 +0100

    custom Auth middleware JWT Verifz

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
-//@route Get api/auth
-//@desc test route
+const User = require('../../models/User');
+
+//@route GET api/auth
+//@desc Test route
 //@access Public
-router.get('/', (req, res) => res.send('auth route'))
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+        
+
+    
 
 module.exports = router;
